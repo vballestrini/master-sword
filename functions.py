68717d74eaa819c8631dbe8fb929d5d64950aa45
Víctor Ballestrini <vsballestrini@gmail.com>
2020-10-08 00:23:24 -0300

    Inicia função de busca

--- functions.py
+++ functions.py
@@ -1,22 +1,68 @@
 import copy
+from queue import PriorityQueue
 
 #Variaveis globais
-cgrama = 10
-careia = 20
-cfloresta= 100
-cmontanha = 150
-cagua = 180
+GRAMA = 10
+AREIA = 20
+FLORESTA= 100
+MONTANHA = 150
+AGUA = 180
+OBJETIVO = (0, 0)
 
 def leMapa(nomeArquivo):
     mapa = open(nomeArquivo).read()
     mapa = [item.split() for item in mapa.split('\n')[:-1]]
     return mapa
 
+MAPA = leMapa('mapa-hyrule.txt')
+
+class Terreno:
+    def __init__(self, linha, coluna):
+        self.tipo = MAPA[linha][coluna]
+        self.vizinhos = []
+        self.anterior = Terreno
+        self.linha = linha
+        self.coluna = coluna
+
+    def getPosicao(self):
+        return self.linha, self.coluna
+
+    def getCusto(self):
+        g, h = 0, 0
+        if self.tipo == 'G':
+            g = 10
+        elif self.tipo == 'A':
+            g = 20
+        elif self.tipo == 'F':
+            g = 100
+        elif self.tipo == 'M':
+            g = 150
+        elif self.tipo == 'H':
+            g = 180
+        x1, y1 = self.linha, self.coluna
+        x2, y2 = OBJETIVO
+        h = abs(x1 - x2) + abs(y1 - y2)
+        return g + h
+
+    def setAnterior(self, terreno):
+        self.anterior = terreno
+
+
+
 def imprimeMapa(mapa):
     linhas = len(mapa)
     colunas = len(mapa[0])
-    
     for i in range(linhas):
         for j in range(colunas):
             print('%s' %mapa[i][j], end = '')
         print()
+
+def encontraCaminho (casa, msword, pverde, pazul, pvermelho):
+    
+    global OBJETIVO
+    objetivo = pverde
+    expandidos = {casa}
+    visitados = {}
+
+    while expandidos:
+        pass
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import functions 
 
-#Monta matriz
-mapa = functions.leMapa('mapa-hyrule.txt')
-functions.imprimeMapa(mapa)
+casa = (27, 23) #posição inicial do link
+msword = (1, 2) #ponto de chegana na master sword
+pverde = (32, 5) #posição do pingente verde
+pazul = (17, 39) #posição do pingente azul
+pvermelho = (1, 24) #posição do pingente vermelho
+
+functions.encontraCaminho(casa, msword, pverde, pazul, pvermelho)
+
